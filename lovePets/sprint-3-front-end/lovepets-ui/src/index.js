a09d33306ec3f42e5229fb1b9f348fafcdcd5968
FIC <FIC@info.local>
2021-07-21 09:12:33 -0300

    Pasta reforço add

---
+++ lovePets/sprint-3-front-end/lovepets-ui/src/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+
+import { usuarioAutenticado, parseJwt } from './services/auth';
+
+import './index.css';
+
+import App from './pages/home/App';
+import Atendimentos from './pages/atendimentos/atendimentos';
+import Login from './pages/login/login';
+import NotFound from './pages/notFound/notFound';
+
+import reportWebVitals from './reportWebVitals';
+
+      // Estrutura do IF Ternário
+      // CONDIÇÃO ? FAÇO ALGO SE VERDADEIRO : FAÇO ALGO SE FALSO
+      // Exemplo:
+      // 2 > 3 ? Sim : Não
+      // resposta : Não
+
+const PermissaoAdm = ({ component : Component }) => (
+  <Route 
+    render = { props => 
+      usuarioAutenticado() && parseJwt().role === "1" ?
+      <Component {...props} /> :
+      <Redirect to="/login" />
+    }
+  />
+)
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <PermissaoAdm path="/atendimentos" component={Atendimentos} />
+        <Route path="/login" component={Login} />
+        <Route exact path="/notfound" component={NotFound} />
+        <Redirect to="/notfound" />
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
